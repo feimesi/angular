394e7db74ea15aedc2838de33a47bc2ca33ca1b6
867695377@qq.com <867695377@qq.com>
2018-12-25 23:36:10 +0900

    问题1，3对应

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableshComponent } from './common/tablesh/tablesh.component';
+import { LoginComponent } from './common/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'tablesh', component: TableshComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './common/login/login.component';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { TableshComponent } from './common/tablesh/tablesh.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatTableModule, MatTabsModule, MatSortModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatTableModule, MatTabsModule, MatSortModule, MatIconModule} from '@angular/material';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '.././assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -19,8 +25,17 @@ import {MatButtonModule, MatCheckboxModule, MatTableModule, MatTabsModule, MatSo
     AppRoutingModule,
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatIconModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      }
+  }),
     MatSortModule,
     MatTableModule,
     MatTabsModule,
